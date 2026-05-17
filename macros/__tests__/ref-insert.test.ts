dfe37e4d2b40255d95e336aedc101d6bcd4d1d82
8kto <ifhtagn@gmail.com>
2026-05-17 13:21:22 +0200

    Fix tests

--- macros/__tests__/ref-insert.test.ts
+++ macros/__tests__/ref-insert.test.ts
@@ -1,22 +1,26 @@
+import type { IDocumentConfig } from '../../types'
 import { convertRefInserts } from '../ref.macro'
+
+const config = {} as IDocumentConfig
 
 describe('convert ref inserts', () => {
   it('should convert cmd into shortened stats html', () => {
-    expect(convertRefInserts(`<!-- cmd[ref] header[Костяная химера] /-->`)).toMatchSnapshot()
+    expect(convertRefInserts(`<!-- cmd[ref] header[Костяная химера] /-->`, config)).toMatchSnapshot()
   })
 
   it('should convert cmd into full html', () => {
-    expect(convertRefInserts(`<!-- cmd[ref] header[Костяная химера] detailed /-->`)).toMatchSnapshot()
+    expect(convertRefInserts(`<!-- cmd[ref] header[Костяная химера] detailed /-->`, config)).toMatchSnapshot()
   })
 
   it('should ignore not found refs', () => {
-    expect(convertRefInserts(`<!-- cmd[ref] header[XXX YYY] /-->`)).toEqual(`<!-- cmd[ref] header[XXX YYY] /-->`)
+    expect(convertRefInserts(`<!-- cmd[ref] header[XXX YYY] /-->`, config)).toEqual(`<!-- cmd[ref] header[XXX YYY] /-->`)
   })
 
   it('should insert n times', () => {
     expect(
       convertRefInserts(
         `XXX <!-- cmd[ref] header[Костяная химера] /--> YYY <!-- cmd[ref] header[Костяная химера] /-->`,
+        config,
       ),
     ).toMatchSnapshot()
   })
