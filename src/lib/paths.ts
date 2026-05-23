57c0af29814fb3d166ed3067110e80f1372ec3ef
8kto <ifhtagn@gmail.com>
2026-05-23 20:37:39 +0200

    Cleanup

--- src/lib/paths.ts
+++ src/lib/paths.ts
@@ -52,6 +52,7 @@ export const getPdfBuildPath = (config: IDocumentConfig) => {
   return path.join(config.outputPath, 'pdf', `module-${config.id}`)
 }
 
+// TODO integrate into resolveContentPaths
 export const getCssPath = <T extends { outputPath: string; cssPath?: string }>(config: T) => {
   return config.cssPath ?? path.join(config.outputPath, 'output.css')
 }
